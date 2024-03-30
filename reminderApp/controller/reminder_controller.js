27aab55338f3c6a294cc95d73f87e5342b40282f
swimmingwebdev <swimmingwebdev@gmail.com>
2024-03-30 13:30:34 -0700

    edit reminder_controller.js

--- reminderApp/controller/reminder_controller.js
+++ reminderApp/controller/reminder_controller.js
@@ -1,16 +1,19 @@
 let database = require("../database");
 
 let remindersController = {
+  // get
   list: (req, res) => {
     res.render("reminder/index", {
       reminders: req.user.reminders,
     });
   },
 
+  // get
   new: (req, res) => {
     res.render("reminder/create");
   },
 
+  // get
   listOne: (req, res) => {
     let reminderToFind = req.params.id;
     let searchResult = req.user.reminders.find(function (reminder) {
@@ -23,6 +26,7 @@ let remindersController = {
     }
   },
 
+  // post
   create: (req, res) => {
     let reminder = {
       id: req.user.reminders.length + 1,
@@ -31,9 +35,10 @@ let remindersController = {
       completed: false,
     };
     req.user.reminders.push(reminder);
-    res.redirect("/reminders");
+    res.redirect("/reminder/index");
   },
 
+  // get
   edit: (req, res) => {
     let reminderToFind = req.params.id;
     let searchResult = req.user.reminders.find(function (reminder) {
@@ -42,12 +47,24 @@ let remindersController = {
     res.render("reminder/edit", { reminderItem: searchResult });
   },
 
+  // post
   update: (req, res) => {
-    // implementation here 👈
+    let reminderToUpdate = req.params.id - 1;
+    req.user.reminders[reminderToUpdate].title = req.body.title;
+    req.user.reminders[reminderToUpdate].description = req.body.description;
+    req.user.reminders[reminderToUpdate].completed =
+      req.body.completed === "true";
+    res.redirect("/reminder/index");
   },
 
+  // post
   delete: (req, res) => {
-    // implementation here 👈
+    let reminderToDelete = req.params.id;
+    let searchResult = req.user.reminders.find(function (reminder) {
+      return reminder.id == reminderToDelete;
+    });
+    req.user.reminders.splice(searchResult.id - 1, 1);
+    res.redirect("/reminder/index");
   },
 };
 
